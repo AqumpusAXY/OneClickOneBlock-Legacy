40cfba9a2d8da390d07ca1e9a0a81aefcb902cd9
AqumpusAXY <whitefroth@163.com>
2026-01-14 21:11:10 +0800

    [feat]: 功能实现

---
+++ src/main/java/github/aqumpusaxy/oneclickoneblock/mixin/PlayerControllerMPMixin.java
@@ -0,0 +1,23 @@
+package github.aqumpusaxy.oneclickoneblock.mixin;
+
+import net.minecraft.client.multiplayer.PlayerControllerMP;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = PlayerControllerMP.class)
+public abstract class PlayerControllerMPMixin {
+    @Shadow
+    private int blockHitDelay;
+
+    @Inject(
+            method = "onPlayerDestroyBlock",
+            at = @At("RETURN")
+    )
+    private void addBlockHitDelay(BlockPos pos, CallbackInfoReturnable<Boolean> cir){
+        this.blockHitDelay = 5;
+    }
+}
